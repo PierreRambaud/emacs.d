201b4ee523079528bf28a5c43f00f808b49a00e0
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2016-10-28 15:06:49 +0200

    Update helm module and add games into gitignore

--- modules/helm-module.el
+++ modules/helm-module.el
@@ -8,7 +8,7 @@
 (require 'helm-git-grep)
 (require 'helm-c-yasnippet)
 
-(setq helm-buffers-favorite-modes (quote (emacs-lisp-mode org-mode php-mode ruby-mode python-mode shell-script-mode))
+(setq helm-buffers-favorite-modes (quote (emacs-lisp-mode php-mode ruby-mode python-mode shell-script-mode twig-mode org-mode))
       helm-follow-mode-persistent t)
 (defun helm-buffers-right-side ()
   "Special helm settings to list buffers in right side."
