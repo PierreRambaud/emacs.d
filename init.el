a9fef096a6c2320b2ad58107b5e72152f9396757
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-05-21 09:36:39 +0200

    Update for emacs 25

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when (version< emacs-version "24.4")
   (error "Configuration requires at least GNU Emacs 24.4, but you're running %s" emacs-version))
 
