0e7a79ab775d8ea11420a2958acecb2d510e27f9
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-01-30 23:24:10 +0100

    Update python indentation

--- modules/python-module.el
+++ modules/python-module.el
@@ -11,6 +11,8 @@
 (elpy-enable)
 (setq elpy-rpc-backend "rope")
 (add-hook 'python-mode-hook (lambda ()
+                              (setq python-indent-offset 4)
+                              (setq python-guess-indent nil)
                               (define-key python-mode-map (kbd "RET") 'newline-and-indent)))
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
