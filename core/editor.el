c8075909707dd0181488631b0077bd11fe9777ee
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-01-21 10:59:24 +0100

    Open neotree based on projectile

--- core/editor.el
+++ core/editor.el
@@ -218,7 +218,19 @@ The body of the advice is in BODY."
 
 ;; Neotree
 (require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
+(global-set-key [f8] 'neotree-project-dir)
 
 (provide 'editor)
 
