4439a7935d8ee0ff636f5c079079fbf4dda55fe1
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-01-23 15:52:40 +0100

    Add markdown mode for GitHub

--- modules/md-module.el
+++ modules/md-module.el
@@ -19,6 +19,15 @@
   (interactive)
   (browse-url-of-buffer (markdown-standalone markdown-output-buffer-name)))
 
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
 (provide 'md-module)
 
 ;;; md-module.el ends here
