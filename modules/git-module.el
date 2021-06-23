b4a70b0b73e1abac2d5100e9111f05b01896a74f
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-06-23 14:29:39 +0200

    Add shortcut for browse-at-remote

--- modules/git-module.el
+++ modules/git-module.el
@@ -37,6 +37,7 @@
 (global-diff-hl-mode +1)
 ;; (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+(global-set-key (kbd "C-c g g") 'browse-at-remote)
 
 (require 'browse-at-remote)
 
