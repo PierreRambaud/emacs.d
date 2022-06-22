0df42af9d82bf9a155613fe5a7da04c213be0c25
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2022-06-22 14:11:10 +0200

    Remove unneeded code because of LSP

--- modules/php-module.el
+++ modules/php-module.el
@@ -7,6 +7,7 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'phpunit)
 (require 'php-mode)
 (require 'php-doc-block)
 (require 'flymake-php)
@@ -16,47 +17,6 @@
 (require 'phpactor)
 
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-
-(add-hook 'php-mode-hook (lambda ()
-                           (require 'company-php)
-                           (company-mode t)
-                           (local-set-key (kbd "<C-tab>") 'php-doc-block)
-                           (make-local-variable 'company-backends)
-                           (add-to-list 'company-backends 'company-files)
-                           (subword-mode)
-                           (flymake-php-load)
-                           (c-set-offset 'case-label '+)
-                           (define-key php-mode-map (kbd "RET") 'newline-and-indent)))
-
-
-(define-transient-command php-transient-menu ()
-  "Php"
-  [["Class"
-    ("cc" "Copy" phpactor-copy-class)
-    ("cn" "New" phpactor-create-new-class)
-    ("cr" "Move" phpactor-move-class)
-    ("ci" "Inflect" phpactor-inflect-class)
-    ("n"  "Namespace" phpactor-fix-namespace)]
-   ["Properties"
-    ("a"  "Accessor" phpactor-generate-accessors)
-    ("pc" "Constructor" phpactor-complete-constructor)
-    ("pm" "Add missing props" phpactor-complete-properties)
-    ("r" "Rename var locally" phpactor-rename-variable-local)
-    ("R" "Rename var in file" phpactor-rename-variable-file)]
-  ["Extract"
-    ("ec" "constant" phpactor-extract-constant)
-    ("ee" "expression" phpactor-extract-expression)
-    ("em"  "method" phpactor-extract-method)]
-  ["Methods"
-    ("i" "Implement Contracts" phpactor-implement-contracts)
-    ("m"  "Generate method" phpactor-generate-method)]
-  ["Navigate"
-    ("x" "List refs" phpactor-list-references)
-    ("X" "Replace refs" phpactor-replace-references)
-    ("."  "Goto def" phpactor-goto-definition)]
-  ["Phpactor"
-    ("s" "Status" phpactor-status)
-    ("u" "Install" phpactor-install-or-update)]])
 
 (setq phpunit-stop-on-failure t)
 (setq phpunit-colorize "auto")
